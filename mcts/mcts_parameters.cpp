e15f8795f5e2fd595f8f96d64c76de4f854ce5b7
Luis Gressenbuch <gressenbuch@fortiss.org>
2019-10-23 10:33:13 +0200

    Adjust parameters

--- mcts/mcts_parameters.cpp
+++ mcts/mcts_parameters.cpp
@@ -11,12 +11,12 @@ namespace mcts {
 double MctsParameters::DISCOUNT_FACTOR = 1;
 double MctsParameters::EXPLORATION_CONSTANT = 1;
 
-double MctsParameters::MAX_SEARCH_TIME_RANDOM_HEURISTIC = 100;
+double MctsParameters::MAX_SEARCH_TIME_RANDOM_HEURISTIC = 1000;
 double MctsParameters::MAX_NUMBER_OF_ITERATIONS_RANDOM_HEURISTIC = 1000;
 
-ObjectiveVec MctsParameters::LOWER_BOUND = Eigen::Vector4f(-1000.0f, -1000.0f, -1000.0f, -1000.0f);
-ObjectiveVec MctsParameters::UPPER_BOUND = Eigen::Vector4f(0.0f, 0.0f, 0.0f, 0.0f);
+ObjectiveVec MctsParameters::LOWER_BOUND = Eigen::Vector4f(-2000.0f, -1000.0f, -1000.0f, -1000.0f);
+ObjectiveVec MctsParameters::UPPER_BOUND = Eigen::Vector4f(2000.0f, 0.0f, 0.0f, 0.0f);
 
-double MctsParameters::COOP_FACTOR = 0.0f;
+double MctsParameters::COOP_FACTOR = 0;
 
 } // namespace mcts
