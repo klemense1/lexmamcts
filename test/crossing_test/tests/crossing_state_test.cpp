cf61a8e2bb420e801c8268ec66d562370a92d192
Luis Gressenbuch <gressenbuch@fortiss.org>
2019-10-23 10:32:13 +0200

    Fix broken formatting

--- test/crossing_test/tests/crossing_state_test.cpp
+++ test/crossing_test/tests/crossing_state_test.cpp
@@ -25,94 +25,64 @@ class CrossingTestF : protected CrossingTest, public ::testing::Test {
     CrossingTestF() : CrossingTest() {};
 };
 
-TEST_F(CrossingTestF, general
-) {
-const int MAX_STEPS = 40;
-int steps = 0;
-pos_history.
-emplace_back(state
-->
-get_ego_pos()
-);
-while (!state->
-is_terminal() &&
-steps<MAX_STEPS) {
-mcts.
-search(*state,
-50000, 1000);
-//jt[0] = mcts.returnBestAction()[0];
-jt = mcts.returnBestAction();
-state = state->execute(jt, rewards);
-pos_history.
-emplace_back(state
-->
-get_ego_pos()
-);
-++
-steps;
+TEST_F(CrossingTestF, general) {
+    const int MAX_STEPS = 40;
+    int steps = 0;
+    pos_history.emplace_back(state->get_ego_pos());
+    while (!state->is_terminal() && steps < MAX_STEPS) {
+        mcts.search(*state,50000, 1000);
+        //jt[0] = mcts.returnBestAction()[0];
+        jt = mcts.returnBestAction();
+        state = state->execute(jt, rewards);
+        pos_history.emplace_back(state->get_ego_pos());
+        ++steps;
+    }
+
+    std::cout << "Ego positions:" <<
+    std::endl;
+    for (auto p: pos_history) {
+        std::cout << p << ", ";
+    }
+
+    //Should not hit the maximum # of steps
+    EXPECT_LT(steps, MAX_STEPS);
+    EXPECT_TRUE(state->ego_goal_reached());
 }
 
-std::cout << "Ego positions:" <<
-std::endl;
-for (
-auto p
-: pos_history) {
-std::cout << p << ", ";
+TEST_F(CrossingTestF, giveWay) {
+    std::vector<AgentState> agent_states(2);
+    agent_states[0].x_pos = 7;
+    agent_states[0].last_action = Actions::FORWARD;
+    agent_states[1].x_pos = 8;
+    agent_states[1].last_action = Actions::FORWARD;
+    state = std::make_shared<CrossingState>(agent_states,
+                                            false,
+                                            automata,
+                                            label_evaluators);
+    state = state->execute(jt, rewards);
+    state = state->execute(jt, rewards);
+    state = state->execute(jt, rewards);
+    state = state->execute(jt, rewards);
+    ASSERT_EQ(rewards[0](0),-500);
+    state->execute(jt, rewards);
 }
 
-//Should not hit the maximum # of steps
-EXPECT_LT(steps, MAX_STEPS
-);
-EXPECT_TRUE(state
-->
-ego_goal_reached()
-);
-}
-
-TEST_F(CrossingTestF, giveWay
-) {
-std::vector<AgentState> agent_states(2);
-agent_states[0].
-x_pos = 7;
-agent_states[0].
-last_action = Actions::FORWARD;
-agent_states[1].
-x_pos = 8;
-agent_states[1].
-last_action = Actions::FORWARD;
-state = std::make_shared<CrossingState>(agent_states,
-                                        false,
-                                        automata,
-                                        label_evaluators);
-state = state->execute(jt, rewards);
-state = state->execute(jt, rewards);
-state = state->execute(jt, rewards);
-state = state->execute(jt, rewards);
-ASSERT_EQ(rewards[0](0),
--500);
-state->
-execute(jt, rewards
-);
-}
-
-TEST_F(CrossingTestF, LexicographicOrder
-) {
-std::vector<Eigen::Vector2i> v;
-v.emplace_back(1, 2);
-v.emplace_back(2, 1);
-v.emplace_back(2, 3);
-v.emplace_back(2, 2);
-v.emplace_back(-5, 3);
-auto max = std::max_element(v.begin(), v.end(),
-                            [](Eigen::Vector2i &a,
-                               Eigen::Vector2i &b) -> bool {
-                                return std::lexicographical_compare(a.begin(),
-                                                                    a.end(),
-                                                                    b.begin(),
-                                                                    b.end());
-                            });
-ASSERT_EQ(*max, Eigen::Vector2i(2, 3)
-);
+TEST_F(CrossingTestF, LexicographicOrder) {
+    std::vector<Eigen::Vector2i> v;
+    v.emplace_back(1, 2);
+    v.emplace_back(2, 1);
+    v.emplace_back(2, 3);
+    v.emplace_back(2, 2);
+    v.emplace_back(-5, 3);
+    auto max = std::max_element(v.begin(), v.end(),
+                                [](Eigen::Vector2i &a,
+                                   Eigen::Vector2i &b) -> bool {
+                                    return std::lexicographical_compare(a.begin(),
+                                                                        a.end(),
+                                                                        b.begin(),
+                                                                        b.end());
+                                });
+    ASSERT_EQ(*max, Eigen::Vector2i(2, 3));
 }
 
 int main(int argc, char **argv) {
