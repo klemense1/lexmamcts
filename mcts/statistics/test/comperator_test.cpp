447a245b711fd9e25f389d5b775fbe5b7a042de0
Luis Gressenbuch <gressenbuch@fortiss.org>
2019-11-08 12:09:11 +0100

    Move comperator test to statistics dir

--- mcts/statistics/test/comperator_test.cpp
+++ mcts/statistics/test/comperator_test.cpp
@@ -1,13 +1,12 @@
 //
-// Created by luis on 26.10.19.
+// Created by Luis Gressenbuch on 07.11.19.
+// Copyright (c) 2019 Luis Gressenbuch. All rights reserved.
 //
 
-#define UNIT_TESTING
-#define DEBUG
-#define PLAN_DEBUG_INFO
-
 #include "gtest/gtest.h"
-#include "mcts/statistics/uct_statistic.h"
+
+#include "mcts/statistics/slack_uct_statistic.h"
+#include "mcts/statistics/thres_uct_statistic.h"
 
 using namespace mcts;
 
@@ -16,23 +15,31 @@ TEST(ComperatorTest, slack_comperator) {
   ObjectiveVec b = ObjectiveVec::Ones();
   ObjectiveVec slack_a = ObjectiveVec::Zero();
   ObjectiveVec slack_b = ObjectiveVec::Zero();
-  a *= 10;
-  b *= 20;
-  slack_a << 5, 5, 0, 0;
-  slack_b << 2, 2, 0, 0;
-  // No overlap
-  ASSERT_TRUE(slack_compare(a, b, slack_a, slack_b));
-  slack_b << 5, 5, 0, 0;
-  // Overlap
-  ASSERT_TRUE(slack_compare(a, b, slack_a, slack_b));
-  a(2) = 50;
+  a << 0, 0, 0, 0;
+  b << 4, 4, 4, 4;
+  slack_a << 2, 2, 4, 0;
+  slack_b << 2, 3, 0, 4;
+  // a == b
   ASSERT_FALSE(slack_compare(a, b, slack_a, slack_b));
+  ASSERT_FALSE(slack_compare(b, a, slack_b, slack_a));
+  // a < b
+  slack_a << 1, 1, 0, 0;
+  slack_b << 1, 1, 0, 0;
+  ASSERT_TRUE(slack_compare(a, b, slack_a, slack_b));
+  ASSERT_FALSE(slack_compare(b, a, slack_b, slack_a));
+  // a > b
+  a << 4, 4, 4, 4;
+  b << 0, 0, 0, 0;
+  slack_a << 1, 1, 0, 0;
+  slack_b << 1, 1, 0, 0;
+  ASSERT_FALSE(slack_compare(a, b, slack_a, slack_b));
+  ASSERT_TRUE(slack_compare(b, a, slack_b, slack_a));
 }
 
 TEST(ComperatorTest, threshold_comperator_random) {
   ObjectiveVec a = ObjectiveVec::Ones();
   ObjectiveVec b = ObjectiveVec::Ones();
-  ObjectiveVec thr = ObjectiveVec::Ones() * FLT_MAX;
+  ObjectiveVec thr = ObjectiveVec::Constant(std::numeric_limits<float>::max());
   std::mt19937 prng(1000);
   std::uniform_int_distribution<int> distr(-100, 100);
   auto gen = [&distr, &prng]() {
@@ -55,4 +62,11 @@ TEST(ComperatorTest, threshold_comperator) {
   thr << 10, 100, 100, 100;
   ASSERT_FALSE(std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end()));
   ASSERT_TRUE(threshold_compare(a, b, thr));
+}
+
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  ::testing::InitGoogleTest(&argc, argv);
+  FLAGS_logtostderr = 1;
+  return RUN_ALL_TESTS();
 }
