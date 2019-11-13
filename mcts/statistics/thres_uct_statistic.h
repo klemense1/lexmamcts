302cd3ed63c86b86c86b630dfab72caae378882c
Luis Gressenbuch <gressenbuch@fortiss.org>
2019-11-13 15:49:10 +0100

    Also use thresholding when selecting internal nodes

--- mcts/statistics/thres_uct_statistic.h
+++ mcts/statistics/thres_uct_statistic.h
@@ -15,18 +15,6 @@
 
 namespace mcts {
 
-bool threshold_compare(Eigen::VectorXf const &a, Eigen::VectorXf const &b, Eigen::VectorXf const &thr) {
-  assert(a.rows() == b.rows() && a.rows() == thr.rows());
-  for (auto ai = a.begin(), bi = b.begin(), thri = thr.begin(); ai != a.end(); ++ai, ++bi, ++thri) {
-    if ((*ai >= *thri && *bi >= *thri) || (*ai == *bi)) {
-      continue;
-    } else {
-      return *ai < *bi;
-    }
-  }
-  return false;
-}
-
 class ThresUCTStatistic : public UctStatistic<ThresUCTStatistic> {
   typedef NodeStatistic<ThresUCTStatistic> ParentType;
 
@@ -34,19 +22,62 @@ class ThresUCTStatistic : public UctStatistic<ThresUCTStatistic> {
   ThresUCTStatistic(ActionIdx num_actions, MctsParameters const &mcts_parameters) : UctStatistic<ThresUCTStatistic>(
       num_actions,
       mcts_parameters){}
+  struct ThresholdComparator {
+    ThresholdComparator(const Eigen::VectorXf &thr) : thr_(thr) {};
+    const Eigen::VectorXf &thr_;
+    bool operator()(Eigen::VectorXf const &a, Eigen::VectorXf const &b) const {
+      assert(a.rows() == b.rows() && a.rows() == thr_.rows());
+      for (auto ai = a.begin(), bi = b.begin(), thri = thr_.begin(); ai != a.end(); ++ai, ++bi, ++thri) {
+        if ((*ai >= *thri && *bi >= *thri) || (*ai == *bi)) {
+          continue;
+        } else {
+          return *ai < *bi;
+        }
+      }
+      return false;
+    }
+  };
+
+  template<class S>
+  ActionIdx choose_next_action(const S &state, std::vector<int> &unexpanded_actions) {
+    if (unexpanded_actions.empty()) {
+      // Select an action based on the UCB formula
+      std::vector<Eigen::VectorXf> values;
+      calculate_ucb_values(ucb_statistics_, values);
+      ActionIdx selected_action = std::distance(values.begin(),
+                                                std::max_element(values.begin(),
+                                                                 values.end(),
+                                                                 ThresholdComparator(mcts_parameters_.thres_uct_statistic_.THRESHOLD)));;
+      return selected_action;
+    } else {
+      // Select randomly an unexpanded action
+      std::uniform_int_distribution<ActionIdx> random_action_selection(0, unexpanded_actions.size() - 1);
+      ActionIdx array_idx = random_action_selection(random_generator_);
+      ActionIdx selected_action = unexpanded_actions[array_idx];
+      unexpanded_actions.erase(unexpanded_actions.begin() + array_idx);
+      return selected_action;
+    }
+  }
 
   ActionIdx get_best_action() {
+    Eigen::MatrixXf mat(Reward::RowsAtCompileTime, ucb_statistics_.size());
+    for (ActionIdx i = 0; i < mat.cols(); ++i) {
+      mat.col(i) = ucb_statistics_[i].action_value_;
+    }
+
+    //    Reward max_v = mat.rowwise().maxCoeff();
+    //    Reward thr = max_v - max_v.cwiseAbs().cwiseProduct(Reward::Constant(0.2f));
+    Reward thr = mcts_parameters_.thres_uct_statistic_.THRESHOLD;
+    DVLOG(1) << "Thresholds:" << thr.transpose();
     auto max = std::max_element(ucb_statistics_.begin(),
                                 ucb_statistics_.end(),
-                                [this](ActionUCBMap::value_type const &a, ActionUCBMap::value_type const &b) {
+                                [thr](ActionUCBMap::value_type const &a, ActionUCBMap::value_type const &b) {
                                   if (a.second.action_count_ == 0) {
                                     return true;
                                   } else if (b.second.action_count_ == 0) {
                                     return false;
                                   } else {
-                                    return threshold_compare(a.second.action_value_,
-                                                             b.second.action_value_,
-                                                             mcts_parameters_.thres_uct_statistic_.THRESHOLD);
+                                    return (ThresholdComparator(thr))(a.second.action_value_, b.second.action_value_);
                                   }
                                 });
     return max->first;
