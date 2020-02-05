c95a87db38d0d2c64e59249172aedf9e0cd215d0
Luis Gressenbuch <gressenbuch@fortiss.org>
2020-02-05 18:17:55 +0100

    Update full expansion

--- test/crossing_test/tests/full_expand.cpp
+++ test/crossing_test/tests/full_expand.cpp
@@ -9,6 +9,7 @@
 #include <thread>
 #include <future>
 
+#include "test/crossing_test/factories/default_test_env_factory.h"
 #include "test/crossing_test/tests/crossing_test_env.h"
 #include "test/crossing_test/tests/util.h"
 
@@ -54,27 +55,28 @@ bool cmp_lt(JointReward const &a, JointReward const &b) {
 
 unsigned long long node_count = 0;
 
-bool dfs(CrossingStateSPtr root, ActionList const &actions, shared_ptr<QValPair> qval) {
+bool dfs(CrossingStateSPtr root, ActionList const &actions, shared_ptr<QValPair> best_action) {
   ++node_count;
   if (root->is_terminal()) {
-    qval->qval = root->get_final_reward();
-    qval->child = shared_ptr<QValPair>();
+    best_action->qval = root->get_final_reward();
+    best_action->child = shared_ptr<QValPair>();
     return false;
   }
-  JointReward step_reward(root->get_agent_idx().size(), Reward::Zero(qval->qval.size()));
-  JointReward total_reward(root->get_agent_idx().size(), Reward::Zero(qval->qval.size()));
-  JointAction best_action;
+  JointReward step_reward(root->get_agent_idx().size(), Reward::Zero(best_action->qval.size()));
+  JointReward total_reward(root->get_agent_idx().size(), Reward::Zero(best_action->qval.size()));
 
-  qval->qval = JointReward(root->get_agent_idx().size(), Reward::Constant(qval->qval.size(), -std::numeric_limits<Reward::Scalar>::max()));
+  best_action->qval =
+      JointReward(root->get_agent_idx().size(),
+                  Reward::Constant(best_action->qval.size(), -std::numeric_limits<Reward::Scalar>::max()));
   for (auto const &act : actions) {
     CrossingStateSPtr next_state = root->execute(act, step_reward);
-    shared_ptr<QValPair> candidate = make_shared<QValPair>(root->get_agent_idx().size(), qval->qval.size());
+    shared_ptr<QValPair> candidate = make_shared<QValPair>(root->get_agent_idx().size(), best_action->qval.size());
     dfs(next_state, actions, candidate);
     total_reward = candidate->qval + step_reward;
-    if (cmp_lt(qval->qval, total_reward)) {
-      qval->qval = total_reward;
-      qval->action = act;
-      qval->child = candidate;
+    if (cmp_lt(best_action->qval, total_reward)) {
+      best_action->qval = total_reward;
+      best_action->action = act;
+      best_action->child = candidate;
     }
   }
   return true;
@@ -83,17 +85,17 @@ bool dfs(CrossingStateSPtr root, ActionList const &actions, shared_ptr<QValPair>
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
   FLAGS_minloglevel = 1;
-  FLAGS_logtostderr = 1;
-  size_t const num_agents = 2;
-  CrossingTestEnv<> test_env;
-  int const max_depth = test_env.state->get_parameters().terminal_depth_;
-  shared_ptr<QValPair> root = make_shared<QValPair>(num_agents, test_env.mcts_parameters_.REWARD_VEC_SIZE);
-  ActionList actions = possible_actions(static_cast<ActionIdx>(Actions::NUM), num_agents);
+  FLAGS_logtostderr = true;
+  auto test_env = DefaultTestEnvFactory().make_test_env();
+  size_t const num_agents = test_env->crossing_state_parameter_.num_other_agents + 1;
+  int const max_depth = test_env->state->get_parameters().terminal_depth_;
+  shared_ptr<QValPair> root = make_shared<QValPair>(num_agents, test_env->mcts_parameters_.REWARD_VEC_SIZE);
+  ActionList actions = possible_actions(test_env->crossing_state_parameter_.action_map.size(), num_agents);
   double total_nodes = (pow(actions.size(), max_depth + 1) - 1)/ static_cast<double>(actions.size() - 1);
   LOG(WARNING) << "# Actions: " << actions.size();
   LOG(WARNING) << "Total nodes to expand: " << total_nodes;
   auto start = chrono::high_resolution_clock::now();
-  auto fut = async(dfs, test_env.state, actions, root);
+  auto fut = async(dfs, test_env->state, actions, root);
   while (fut.wait_for(chrono::seconds(10)) != future_status::ready) {
     unsigned long long n = node_count;
     double elapsed = chrono::duration_cast<chrono::seconds>(chrono::high_resolution_clock::now() - start).count();
