d88f9c774f633cef60f1ef1591a49f93a06df45c
Luis Gressenbuch <gressenbuch@fortiss.org>
2020-02-17 12:08:33 +0100

    Test zip formula v4

--- ltl/test/zipper_merge_formula_test.cpp
+++ ltl/test/zipper_merge_formula_test.cpp
@@ -47,8 +47,7 @@ TEST_P(ZipperMergeFormula, false_alternation_l) {
   labels[Label("on_ego_lane_x")] = true;
   labels[Label("merged_x")] = true;
   labels[Label("in_direct_front_x")] = true;
-  EXPECT_TRUE(check_violation(&rs, labels));
-  EXPECT_FALSE(check_final(rs));
+  EXPECT_TRUE(check_violation(&rs, labels) || check_final(rs));
 }
 
 // Correct alternation, beginning ego lane
@@ -75,13 +74,74 @@ TEST_P(ZipperMergeFormula, true_alternation_l) {
   labels[Label("on_ego_lane_x")] = true;
   labels[Label("merged_x")] = true;
   labels[Label("in_direct_front_x")] = false;
+  EXPECT_FALSE(check_violation(&rs, labels) || check_final(rs));
+}
+
+// Correct alternation, but other merged in early
+TEST_P(ZipperMergeFormula, true_early_merge) {
+  RuleMonitorSPtr rule = RuleMonitor::make_rule(GetParam(), -1.0, 0);
+  auto rs = rule->make_rule_state()[0];
+  EvaluationMap labels;
+  labels[Label("merged_e")] = false;
+  labels[Label("on_ego_lane_x")] = false;
+  labels[Label("merged_x")] = false;
+  labels[Label("in_direct_front_x")] = false;
   EXPECT_FALSE(check_violation(&rs, labels));
-  EXPECT_FALSE(check_final(rs));
+  labels[Label("merged_e")] = false;
+  labels[Label("on_ego_lane_x")] = true;
+  labels[Label("merged_x")] = false;
+  labels[Label("in_direct_front_x")] = true;
+  EXPECT_FALSE(check_violation(&rs, labels));
+  labels[Label("merged_e")] = false;
+  labels[Label("on_ego_lane_x")] = true;
+  labels[Label("merged_x")] = true;
+  labels[Label("in_direct_front_x")] = true;
+  EXPECT_FALSE(check_violation(&rs, labels));
+  labels[Label("merged_e")] = true;
+  labels[Label("on_ego_lane_x")] = true;
+  labels[Label("merged_x")] = true;
+  labels[Label("in_direct_front_x")] = true;
+  EXPECT_FALSE(check_violation(&rs, labels) || check_final(rs));
+}
+
+// Wrong alternation, ego comes from an ending lane
+TEST_P(ZipperMergeFormula, false_merge_with_lane_change) {
+  RuleMonitorSPtr rule = RuleMonitor::make_rule(GetParam(), -1.0, 0);
+  auto rs = rule->make_rule_state()[0];
+  EvaluationMap labels;
+  labels[Label("merged_e")] = false;
+  labels[Label("on_ego_lane_x")] = true;
+  labels[Label("merged_x")] = false;
+  labels[Label("in_direct_front_x")] = true;
+  EXPECT_FALSE(check_violation(&rs, labels));
+  labels[Label("merged_e")] = false;
+  labels[Label("on_ego_lane_x")] = true;
+  labels[Label("merged_x")] = true;
+  labels[Label("in_direct_front_x")] = true;
+  EXPECT_FALSE(check_violation(&rs, labels));
+  labels[Label("merged_e")] = false;
+  labels[Label("on_ego_lane_x")] = false;
+  labels[Label("merged_x")] = true;
+  labels[Label("in_direct_front_x")] = false;
+  EXPECT_FALSE(check_violation(&rs, labels));
+  labels[Label("merged_e")] = true;
+  labels[Label("on_ego_lane_x")] = false;
+  labels[Label("merged_x")] = true;
+  labels[Label("in_direct_front_x")] = false;
+  EXPECT_FALSE(check_violation(&rs, labels));
+  labels[Label("merged_e")] = true;
+  labels[Label("on_ego_lane_x")] = true;
+  labels[Label("merged_x")] = true;
+  labels[Label("in_direct_front_x")] = true;
+  EXPECT_TRUE(check_violation(&rs, labels) || check_final(rs));
 }
 
 const std::string formulas[] = {
     "G((!merged_e & !merged_x & on_ego_lane_x) -> G((merged_e & merged_x) -> "
-    "!in_direct_front_x))"};
+    "!in_direct_front_x))",
+    "G((!merged_e & !merged_x & in_direct_front_x) -> G((merged_e & merged_x) -> !in_direct_front_x))",
+    "G(!merged_e -> in_direct_front_x) -> G(merged_e -> !in_direct_front_x)",
+    "G((!merged_e & !merged_x) -> in_direct_front_x) -> G(merged_e -> !in_direct_front_x)"};
 
 INSTANTIATE_TEST_CASE_P(ZipperMergeFormulaTest, ZipperMergeFormula,
     testing::ValuesIn(formulas));
