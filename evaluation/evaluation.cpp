0569ee12fc6db099d0a54af645b67a5f258f62d1
Luis Gressenbuch <gressenbuch@fortiss.org>
2020-02-29 14:11:17 +0100

    Fix QValWriter

--- evaluation/evaluation.cpp
+++ evaluation/evaluation.cpp
@@ -10,11 +10,10 @@
 struct ThresholdComparator {
   explicit ThresholdComparator(Eigen::VectorXf thr) : thr_(std::move(thr)) {}
   const Eigen::VectorXf thr_;
-  int operator()(Eigen::VectorXf const &a, Eigen::VectorXf const &b) const {
+  int operator()(Eigen::VectorXf const& a, Eigen::VectorXf const& b) const {
     assert(a.rows() == b.rows() && a.rows() == thr_.rows());
     int i = 0;
-    for (auto ai = a.begin(), bi = b.begin(), thri = thr_.begin();
-         ai != a.end(); ++ai, ++bi, ++thri, ++i) {
+    for (auto ai = a.begin(), bi = b.begin(), thri = thr_.begin(); ai != a.end(); ++ai, ++bi, ++thri, ++i) {
       if ((*ai > *thri && *bi > *thri) || (*ai == *bi)) {
         continue;
       } else {
@@ -25,28 +24,29 @@ struct ThresholdComparator {
   }
 };
 
-QValWriter::QValWriter(const Eigen::VectorXf& thres, const std::string& filename) : thres_(thres), timestamp_(0) {
+QValWriter::QValWriter(Eigen::VectorXf thres, const std::string& filename) : thres_(std::move(thres)), timestamp_(0) {
   ofstream_.open(filename);
 }
 
 void QValWriter::WriteQVal(const std::map<unsigned long, Eigen::VectorXf>& action_val_map, unsigned long best_action) {
   Eigen::IOFormat fmt(Eigen::StreamPrecision, Eigen::DontAlignCols, "\t");
   ofstream_ << timestamp_ << "\t";
-  for(auto const& pair : action_val_map) {
+  for (auto const& pair : action_val_map) {
     ofstream_ << pair.second.transpose().format(fmt) << "\t";
   }
   ofstream_ << best_action << "\t" << FindLexMax(action_val_map, best_action) << "\n";
   ++timestamp_;
 }
 
-QValWriter::~QValWriter() {
-  ofstream_.close();
-}
+QValWriter::~QValWriter() { ofstream_.close(); }
 
 int QValWriter::FindLexMax(const std::map<unsigned long, Eigen::VectorXf>& action_val_map, unsigned long best_action) {
   ThresholdComparator comp(thres_);
   int decision_level = -1;
-  for(const auto& item : action_val_map) {
+  for (const auto& item : action_val_map) {
+    if (best_action == item.first) {
+      continue;
+    }
     decision_level = std::max(decision_level, comp(action_val_map.at(best_action), item.second));
   }
   return decision_level;
