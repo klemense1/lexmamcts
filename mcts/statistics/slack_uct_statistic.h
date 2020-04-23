51878160b72acc2109f51e66d4519cb56c3cc898
Luis Gressenbuch <gressenbuch@fortiss.org>
2020-04-23 20:30:19 +0200

    Fix slack uct

--- mcts/statistics/slack_uct_statistic.h
+++ mcts/statistics/slack_uct_statistic.h
@@ -14,6 +14,7 @@
 #include "glog/logging.h"
 
 #include "mcts/statistics/uct_statistic.h"
+#include "mcts/statistics/threshold_comperator.h"
 
 namespace mcts {
 
@@ -21,39 +22,18 @@ class SlackUCTStatistic : public UctStatistic<SlackUCTStatistic> {
   typedef NodeStatistic<SlackUCTStatistic> ParentType;
 
  public:
-  template <class T>
-  struct SlackComparator {
-    explicit SlackComparator(T slack) : slack_(std::move(slack)) {}
-    bool operator()(const T &a, const T &b) const {
-      assert(a.rows() == b.rows() && a.rows() == slack_.rows() && b.rows() == slack_.rows());
-      auto a_upper = a + slack_;
-      auto a_lower = a - slack_;
-      auto b_upper = b + slack_;
-      auto b_lower = b - slack_;
-      for (long i = 0; i < a_upper.rows(); ++i) {
-        if (a_upper(i) < b_lower(i)) {
-          return true;
-        } else if (b_upper(i) < a_lower(i)) {
-          return false;
-        }
-      }
-      return false;
-    }
-    const T slack_;
-  };
 
   SlackUCTStatistic(ActionIdx num_actions,
                     MctsParameters const &mcts_parameters)
       : UctStatistic<SlackUCTStatistic>(num_actions, mcts_parameters) {}
 
   ActionIdx get_best_action() {
-    // Lexicographical ordering of the UCT value vectors
     ObjectiveVec slack;
     std::vector<ObjectiveVec> qval;
     std::transform(ucb_statistics_.begin(), ucb_statistics_.end(), std::back_inserter(qval),
                    [](const ActionUCBMap::value_type &elem) { return elem.second.action_value_; });
     calculate_slack_values(qval, &slack);
-    SlackComparator<ObjectiveVec> comp(slack);
+    ThresholdComparator<ObjectiveVec> comp(slack);
     VLOG(1) << "Slack: " << slack;
     auto max = std::max_element(ucb_statistics_.begin(), ucb_statistics_.end(),
                                 [comp](ActionUCBMap::value_type const &a,
@@ -73,31 +53,32 @@ class SlackUCTStatistic : public UctStatistic<SlackUCTStatistic> {
   ActionIdx choose_next_action(const S &state,
                                std::vector<int> &unexpanded_actions,
                                unsigned int iteration) {
+    ActionIdx selected_action;
     if (unexpanded_actions.empty()) {
       std::uniform_real_distribution<double> uniform_norm(0.0, 1.0);
       std::vector<Eigen::VectorXd> values;
       Eigen::VectorXd slack;
       calculate_ucb_values(ucb_statistics_, values);
       calculate_slack_values(values, &slack);
-      ActionIdx selected_action = std::distance(
-          values.begin(), std::max_element(values.begin(), values.end(), SlackComparator<Eigen::VectorXd>(slack)));
-      const double epsilon = mcts_parameters_.e_greedy_uct_statistic_.EPSILON;
-      if (uniform_norm(random_generator_) <= 1.0 - epsilon + epsilon / num_actions_) {
-        // Select an action based on the UCB formula
-        return selected_action;
-      } else {
+      selected_action = std::distance(
+          values.begin(), std::max_element(values.begin(), values.end(),
+                                           ThresholdComparator<Eigen::VectorXd>(
+                                               slack)));
+      const double p = uniform_norm(random_generator_);
+      if (p < mcts_parameters_.thres_uct_statistic_.EPSILON && num_actions_ >= 2) {
         std::uniform_int_distribution<ActionIdx> uniform_action(0, num_actions_ - 2);
         ActionIdx random_action = uniform_action(random_generator_);
-        return (random_action == selected_action ? random_action + 1 : random_action);
+        selected_action = (random_action >= selected_action ? random_action + 1 : random_action);
       }
     } else {
       // Select randomly an unexpanded action
-      std::uniform_int_distribution<ActionIdx> random_action_selection(0, unexpanded_actions.size() - 1);
+      std::uniform_int_distribution<ActionIdx> random_action_selection(
+          0, unexpanded_actions.size() - 1);
       ActionIdx array_idx = random_action_selection(random_generator_);
-      ActionIdx selected_action = unexpanded_actions[array_idx];
+      selected_action = unexpanded_actions[array_idx];
       unexpanded_actions.erase(unexpanded_actions.begin() + array_idx);
-      return selected_action;
     }
+    return selected_action;
   }
 
  protected:
@@ -110,7 +91,7 @@ class SlackUCTStatistic : public UctStatistic<SlackUCTStatistic> {
     }
     // After C. Li and K. Czarnecki, “Urban Driving with Multi-Objective Deep Reinforcement Learning,” arXiv:1811.08586
     // [cs], Nov. 2018.
-    *values = mcts_parameters_.slack_uct_statistic_.SLACK_FACTOR * values->cwiseAbs();
+    *values = (1.0  - mcts_parameters_.slack_uct_statistic_.SLACK_FACTOR) * values->cwiseAbs();
   }
 };
 }  // namespace mcts
--- mcts/statistics/test/comperator_test.cpp
+++ mcts/statistics/test/comperator_test.cpp
@@ -6,47 +6,16 @@
 #include "gtest/gtest.h"
 
 #include "mcts/statistics/slack_uct_statistic.h"
-#include "mcts/statistics/thres_uct_statistic.h"
+#include "mcts/statistics/threshold_comperator.h"
 
 using namespace mcts;
-
-TEST(ComperatorTest, slack_comperator) {
-  const size_t reward_vec_size = 5;
-  ActionUCBMap::value_type a = {0, UcbPair(reward_vec_size)};
-  ActionUCBMap::value_type b = {1, UcbPair(reward_vec_size)};
-  std::vector<ObjectiveVec> slack = {ObjectiveVec::Zero(reward_vec_size), ObjectiveVec::Zero(reward_vec_size)};
-  ObjectiveVec &slack_a = slack[0];
-  ObjectiveVec &slack_b = slack[1];
-  a.second.action_value_ << 0, 0, 0, 0, 0;
-  b.second.action_value_ << 4, 4, 4, 4, 4;
-  slack_a << 2, 2, 4, 0, 0;
-  slack_b << 2, 3, 0, 4, 4;
-  // a == b
-  SlackUCTStatistic::SlackComparator slack_compare(slack);
-  ASSERT_FALSE(slack_compare(a, b));
-  ASSERT_FALSE(slack_compare(b, a));
-  // a < b
-  slack_a << 1, 1, 0, 0, 0;
-  slack_b << 1, 1, 0, 0, 0;
-  SlackUCTStatistic::SlackComparator slack_compare1(slack);
-  ASSERT_TRUE(slack_compare1(a, b));
-  ASSERT_FALSE(slack_compare1(b, a));
-  // a > b
-  a.second.action_value_ << 4, 4, 4, 4, 4;
-  b.second.action_value_ << 0, 0, 0, 0, 0;
-  slack_a << 1, 1, 0, 0, 0;
-  slack_b << 1, 1, 0, 0, 0;
-  SlackUCTStatistic::SlackComparator slack_compare2(slack);
-  ASSERT_FALSE(slack_compare2(a, b));
-  ASSERT_TRUE(slack_compare2(b, a));
-}
 
 TEST(ComperatorTest, threshold_comperator_random) {
   const size_t reward_vec_size = 5;
   ObjectiveVec a = ObjectiveVec::Ones(reward_vec_size);
   ObjectiveVec b = ObjectiveVec::Ones(reward_vec_size);
   ObjectiveVec thr = ObjectiveVec::Constant(reward_vec_size, std::numeric_limits<ObjectiveVec::Scalar>::max());
-  ThresUCTStatistic::ThresholdComparator threshold_compare(thr);
+  ThresholdComparator<ObjectiveVec> threshold_compare(thr);
   std::mt19937 prng(1000);
   std::uniform_int_distribution<int> distr(-100, 100);
   auto gen = [&distr, &prng]() {
@@ -61,16 +30,21 @@ TEST(ComperatorTest, threshold_comperator_random) {
 }
 
 TEST(ComperatorTest, threshold_comperator) {
-  size_t vec_size = 5;
+  size_t vec_size = 3;
   ObjectiveVec a = ObjectiveVec::Zero(vec_size);
   ObjectiveVec b = ObjectiveVec::Zero(vec_size);
   ObjectiveVec thr = ObjectiveVec::Zero(vec_size);
-  a << 20, 10, 20, 10, 10;
-  b << 10, 20, 10, 20, 20;
-  thr << 9, 100, 100, 100, 100;
-  ThresUCTStatistic::ThresholdComparator threshold_compare(thr);
-  ASSERT_FALSE(std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end()));
-  ASSERT_TRUE(threshold_compare(a, b));
+  a << 10, 5, 10;
+  b << 5, 10, 15;
+  thr << 0, 0, 0;
+  ThresholdComparator<ObjectiveVec> threshold_compare(thr);
+  ASSERT_FALSE(threshold_compare(a, b));
+
+  a << 10, 5, 10;
+  b << 5, 10, 15;
+  thr << 0, 0, 20;
+  ThresholdComparator<ObjectiveVec> threshold_compare_2(thr);
+  ASSERT_TRUE(threshold_compare_2(a, b));
 }
 
 int main(int argc, char **argv) {
